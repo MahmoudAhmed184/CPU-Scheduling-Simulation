67a6e786586fd15fb23cb5e0d1d2935d793e3b35
Mahmoud Ahmed <mahmoudxbahnasawy@outlook.com>
2024-10-18 10:26:15 +0300

    feat: implement ScheduledProcess class to encapsulate process scheduling details
    - Implemented ScheduledProcess class to store detailed statistics for scheduled processes.
    - Includes fields for start execution time, completion time, turnaround time, and waiting time.
    - Provides getters for accessing scheduling data.
    - Added a toList method for easy conversion of process statistics to a structured format for display.
    - Enhances separation of concerns by abstracting away scheduling data from algorithm logic.

--- src/main/java/com/mahmoud_ahmed/model/ScheduledProcess.java
+++ src/main/java/com/mahmoud_ahmed/model/ScheduledProcess.java
@@ -1,5 +1,49 @@
 package com.mahmoud_ahmed.model;
 
+import java.util.List;
+
 public class ScheduledProcess {
-    
+    private final Process process;
+    private final int startExecutionTime;
+    private final int completionTime;
+    private final int turnaroundTime;
+    private final int waitingTime;
+
+    public ScheduledProcess(Process process, int startExecutionTime, int completionTime, int turnaroundTime,
+            int waitingTime) {
+        this.process = process;
+        this.startExecutionTime = startExecutionTime;
+        this.completionTime = completionTime;
+        this.turnaroundTime = turnaroundTime;
+        this.waitingTime = waitingTime;
+    }
+
+    public Process getProcess() {
+        return this.process;
+    }
+
+    public int getStartExecutionTime() {
+        return this.startExecutionTime;
+    }
+
+    public int getCompletionTime() {
+        return this.completionTime;
+    }
+
+    public int getTurnaroundTime() {
+        return this.turnaroundTime;
+    }
+
+    public int getWaitingTime() {
+        return this.waitingTime;
+    }
+
+    public List<String> toList() {
+        return List.of(
+                Integer.toString(process.getProcessNumber()),
+                Integer.toString(startExecutionTime),
+                Integer.toString(completionTime),
+                Integer.toString(turnaroundTime),
+                Integer.toString(waitingTime));
+    }
 }
