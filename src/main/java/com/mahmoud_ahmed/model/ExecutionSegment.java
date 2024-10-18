265f5f02c2b92be156a4a7eb18053adafd8d4b08
Mahmoud Ahmed <mahmoudxbahnasawy@outlook.com>
2024-10-18 10:20:32 +0300

    feat: implement ExecutionSegment class to track process execution details
    - Implemented ExecutionSegment class to represent segments of process execution within a time slice.
    - Includes fields for process, start time, and end time.
    - ExecutionSegment instances are created during Preemptive scheduling to track each process’s execution window.
    - Provides a toList method to convert execution segment data into a list format for easy display.

--- src/main/java/com/mahmoud_ahmed/model/ExecutionSegment.java
+++ src/main/java/com/mahmoud_ahmed/model/ExecutionSegment.java
@@ -1,5 +1,34 @@
 package com.mahmoud_ahmed.model;
 
-public class ExecutionSegment {
+import java.util.List;
 
+public class ExecutionSegment {
+    private final Process process;
+    private final int startTime;
+    private final int endTime;
+
+    public ExecutionSegment(Process process, int startTime, int endTime) {
+        this.process = process;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public Process getProcess() {
+        return this.process;
+    }
+
+    public int getStartTime() {
+        return this.startTime;
+    }
+
+    public int getEndTime() {
+        return this.endTime;
+    }
+
+    public List<String> toList() {
+        return List.of(
+                Integer.toString(process.getProcessNumber()),
+                Integer.toString(startTime),
+                Integer.toString(endTime));
+    }
 }
