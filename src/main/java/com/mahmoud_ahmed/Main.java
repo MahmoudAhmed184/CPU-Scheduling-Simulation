c9b0a5656a2ab8f8277542bff0dd84b34ac22bb3
Mahmoud Ahmed <mahmoudxbahnasawy@outlook.com>
2024-10-18 12:13:22 +0300

    feat: demonstrate SchedulingEngine functionality in main

--- src/main/java/com/mahmoud_ahmed/Main.java
+++ src/main/java/com/mahmoud_ahmed/Main.java
@@ -1,7 +1,19 @@
 package com.mahmoud_ahmed;
 
+import java.util.List;
+
+import com.mahmoud_ahmed.simulation.SchedulingEngine;
+import com.mahmoud_ahmed.model.Process;
+
 public class Main {
     public static void main(String[] args) {
-        
+        SchedulingEngine schedulingEngine = new SchedulingEngine();
+        schedulingEngine.runRoundRobin(
+                List.of(
+                        new Process(1, 0, 7, 0),
+                        new Process(2, 2, 4, 0),
+                        new Process(3, 4, 1, 0),
+                        new Process(4, 5, 4, 0)),
+                4);
     }
 }
