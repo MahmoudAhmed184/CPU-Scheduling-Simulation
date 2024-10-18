bb1746dab3eca2563603b1d37329e82ec8e7cacd
Mahmoud Ahmed <mahmoudxbahnasawy@outlook.com>
2024-10-18 12:02:28 +0300

    feat: implement SchedulingEngine class to manage scheduling algorithms
    - Implemented the SchedulingEngine class to facilitate the execution of various scheduling algorithms.
    - Provided methods to run the following algorithms:
      - `runFirstComeFirstServe`: Executes the FCFS scheduling and displays results.
      - `runShortestJobFirst`: Executes the SJF scheduling and displays results.
      - `runNonPreemptivePriority`: Executes the non-preemptive priority scheduling and displays results.
      - `runRoundRobin`: Implements the round robin scheduling with a specified time quantum and displays results.
      - `runPreemptivePriority`: Executes the preemptive priority scheduling and displays results.
    - Utilized the Scheduler class for algorithm implementations and the DisplayManager for presenting results.

--- src/main/java/com/mahmoud_ahmed/simulation/SchedulingEngine.java
+++ src/main/java/com/mahmoud_ahmed/simulation/SchedulingEngine.java
@@ -1,5 +1,43 @@
 package com.mahmoud_ahmed.simulation;
 
+import java.util.Collection;
+import java.util.List;
+import com.mahmoud_ahmed.model.ExecutionSegment;
+import com.mahmoud_ahmed.model.Process;
+import com.mahmoud_ahmed.model.ScheduledProcess;
+import com.mahmoud_ahmed.scheduling.Scheduler;
+import com.mahmoud_ahmed.ui.DisplayManager;
+
 public class SchedulingEngine {
+    private DisplayManager displayManager;
+
+    public SchedulingEngine() {
+        this.displayManager = new DisplayManager();
+    }
+
+    public void runFirstComeFirstServe(Collection<Process> processes) {
+        List<ScheduledProcess> result = Scheduler.scheduleFirstComeFirstServe(processes);
+        displayManager.displayNonPreemptiveAlgorithmResults(result);
+    }
+
+    public void runShortestJobFirst(Collection<Process> processes) {
+        List<ScheduledProcess> result = Scheduler.scheduleShortestJobFirst(processes);
+        displayManager.displayNonPreemptiveAlgorithmResults(result);
+    }
+
+    public void runNonPreemptivePriority(Collection<Process> processes) {
+        List<ScheduledProcess> result = Scheduler.scheduleNonPreemptivePriority(processes);
+        displayManager.displayNonPreemptiveAlgorithmResults(result);
+    }
+
+    public void runRoundRobin(Collection<Process> processes, int timeQuantum) {
+        List<ExecutionSegment> result = Scheduler.scheduleRoundRobin(processes, timeQuantum);
+        displayManager.displayPreemptiveAlgorithmResults(result);
+    }
+
+    public void runPreemptivePriority(Collection<Process> processes) {
+        List<ExecutionSegment> result = Scheduler.schedulePreemptivePriority(processes);
+        displayManager.displayPreemptiveAlgorithmResults(result);
+    }
 
 }
