62539345a1ebc6b8821d40b2ed80967667ac9e25
Mahmoud Ahmed <mahmoudxbahnasawy@outlook.com>
2024-10-18 10:20:19 +0300

    feat: implement Process class with scheduling attributes and methods
    - Implemented Process class with fields for process ID, arrival time, burst time, priority, and remaining time.
    - Added constructor to initialize process properties.
    - Provided methods to get and set remaining time, along with a method to reset remaining time.
    - Implemented compareTo method to compare processes based on arrival time, then process ID.

--- src/main/java/com/mahmoud_ahmed/model/Process.java
+++ src/main/java/com/mahmoud_ahmed/model/Process.java
@@ -1,5 +1,58 @@
 package com.mahmoud_ahmed.model;
 
-public class Process {
+public final class Process implements Comparable<Process> {
+    private static long counter = 0;
+    private final long processId;
+    private final int processNumber;
+    private final int arrivalTime;
+    private final int burstTime;
+    private final int priority;
+    private int remainingTime;
 
+    public Process(int processNumber, int arrivalTime, int burstTime, int priority) {
+        this.processId = counter++;
+        this.processNumber = processNumber;
+        this.arrivalTime = arrivalTime;
+        this.burstTime = burstTime;
+        this.priority = priority;
+        this.remainingTime = burstTime;
+    }
+
+    public long getProcessId() {
+        return this.processId;
+    }
+
+    public int getProcessNumber() {
+        return this.processNumber;
+    }
+
+    public int getArrivalTime() {
+        return this.arrivalTime;
+    }
+
+    public int getBurstTime() {
+        return this.burstTime;
+    }
+
+    public int getPriority() {
+        return this.priority;
+    }
+
+    public int getRemainingTime() {
+        return this.remainingTime;
+    }
+
+    public void setRemainingTime(int remainingTime) {
+        this.remainingTime = remainingTime;
+    }
+
+    public void resetRemainingTime() {
+        this.remainingTime = this.burstTime;
+    }
+
+    @Override
+    public int compareTo(Process that) {
+        int res = Integer.compare(this.getArrivalTime(), that.getArrivalTime());
+        return res != 0 ? res : Long.compare(this.getProcessId(), that.getProcessId());
+    }
 }
