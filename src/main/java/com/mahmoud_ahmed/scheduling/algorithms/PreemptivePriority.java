58932556fb6b014cd2a5ee7622b865e9eb97d403
Mahmoud Ahmed <mahmoudxbahnasawy@outlook.com>
2024-12-23 17:39:22 +0200

    refactor: Simplify PreemptivePriority by extending PreemptiveAlgorithm
    - Refactored `PreemptivePriority` to extend `PreemptiveAlgorithm`, utilizing the shared logic for scheduling processes, idle state management, and ready queue handling.
    - Implemented the `shouldPreempt` method in `PreemptivePriority` to define specific preemption criteria based on process priority.
    - Simplified `PreemptivePriority` class, focusing on algorithm-specific behavior while leveraging the abstract class for shared preemptive scheduling functionality.

--- src/main/java/com/mahmoud_ahmed/scheduling/algorithms/PreemptivePriority.java
+++ src/main/java/com/mahmoud_ahmed/scheduling/algorithms/PreemptivePriority.java
@@ -1,63 +1,18 @@
 package com.mahmoud_ahmed.scheduling.algorithms;
 
 import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.PriorityQueue;
-import java.util.Queue;
-
 import com.mahmoud_ahmed.model.Process;
-import com.mahmoud_ahmed.model.SchedulingClock;
-import com.mahmoud_ahmed.model.ExecutionSegment;
-import com.mahmoud_ahmed.utils.SchedulingUtil;
 
-public class PreemptivePriority implements SchedulingAlgorithm {
-    private final Queue<Process> readyQueue;
+public class PreemptivePriority extends PreemptiveAlgorithm {
 
     public PreemptivePriority() {
-        readyQueue = new PriorityQueue<>(Comparator.comparingInt(Process::getPriority)
-            .thenComparing(Comparator.naturalOrder()));
+        super(new PriorityQueue<>(Comparator.comparingInt(Process::getPriority)
+            .thenComparing(Comparator.naturalOrder())));
     }
 
     @Override
-    public List<ExecutionSegment> schedule(List<Process> processes) {
-        List<Process> sortedProcesses = SchedulingUtil.sortedProcessesByArrivalTime(processes);
-        List<ExecutionSegment> executionSegments = new LinkedList<>();
-
-        Process runningProcess = null;
-        SchedulingClock clock = new SchedulingClock();
-        int startExecutionTime = 0;
-
-        while (!sortedProcesses.isEmpty() || !readyQueue.isEmpty() || runningProcess != null) {
-            SchedulingUtil.addArrivedProcessesToReadyQueue(sortedProcesses, readyQueue, clock);
-
-            if (SchedulingUtil.isCpuInIdleState(sortedProcesses, readyQueue, runningProcess)) {
-                SchedulingUtil.handleIdleState(clock, sortedProcesses.getFirst());
-                continue;
-            }
-
-            if (!readyQueue.isEmpty() && shouldPreempt(runningProcess, readyQueue.peek())) {
-                executionSegments.add(new ExecutionSegment(runningProcess, startExecutionTime, clock.getCurrentTime()));
-                readyQueue.add(runningProcess);
-                runningProcess = null;
-            }
-
-            if (runningProcess == null && !readyQueue.isEmpty()) {
-                runningProcess = readyQueue.poll();
-                startExecutionTime = clock.getCurrentTime();
-            }
-            clock.increment();
-            runningProcess.setRemainingTime(runningProcess.getRemainingTime() - 1);
-            if (runningProcess.getRemainingTime() == 0) {
-                executionSegments.add(new ExecutionSegment(runningProcess, startExecutionTime, clock.getCurrentTime()));
-                runningProcess.resetRemainingTime();
-                runningProcess = null;
-            }
-        }
-        return executionSegments;
-    }
-
-    private static boolean shouldPreempt(Process activeProcess, Process arrivedProcess) {
+    boolean shouldPreempt(Process activeProcess, Process arrivedProcess) {
         return activeProcess != null && activeProcess.getPriority() > arrivedProcess.getPriority();
     }
 }
