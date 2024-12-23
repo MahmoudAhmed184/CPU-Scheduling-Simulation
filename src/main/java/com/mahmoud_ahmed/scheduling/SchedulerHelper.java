558041dd0b072686d1f9ef353d1326b7a7714a8c
Mahmoud Ahmed <mahmoudxbahnasawy@outlook.com>
2024-12-23 17:05:37 +0200

    refactor: Update `scheduleProcess` method to Return ExecutionSegment and Expose Helper Methods
    - Modified `scheduleProcess` method to return an `ExecutionSegment` containing `startExecutionTime` and `completionTime`, removing the calculation of turnaround and waiting times, as they are no longer needed.
    - Refactored method visibility for `sortProcessesByArrivalTime`, `scheduleProcess`, `isCpuInIdleState`, `handleIdleState`, `handleTimeQuantumExpiration`, `shouldPreempt`, and `addArrivedProcessesToReadyQueue` to `public`.

--- src/main/java/com/mahmoud_ahmed/scheduling/SchedulerHelper.java
+++ src/main/java/com/mahmoud_ahmed/scheduling/SchedulerHelper.java
@@ -6,8 +6,8 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
+import com.mahmoud_ahmed.model.ExecutionSegment;
 import com.mahmoud_ahmed.model.Process;
-import com.mahmoud_ahmed.model.ScheduledProcess;
 import com.mahmoud_ahmed.model.SchedulingClock;
 
 public class SchedulerHelper {
@@ -15,44 +15,42 @@ public class SchedulerHelper {
 
     }
 
-    static List<Process> sortProcessesByArrivalTime(Collection<Process> processes) {
+    public static List<Process> sortProcessesByArrivalTime(Collection<Process> processes) {
         List<Process> sortedProcesses = new LinkedList<>(processes);
         Collections.sort(sortedProcesses);
         return sortedProcesses;
     }
 
-    static ScheduledProcess scheduleProcess(Process process, SchedulingClock clock) {
+    public static ExecutionSegment scheduleProcess(Process process, SchedulingClock clock) {
         int startExecutionTime = clock.getCurrentTime();
         int completionTime = startExecutionTime + process.getBurstTime();
-        int turnaroundTime = completionTime - process.getArrivalTime();
-        int waitingTime = turnaroundTime - process.getBurstTime();
         clock.advance(process.getBurstTime());
-        return new ScheduledProcess(process, startExecutionTime, completionTime, turnaroundTime, waitingTime);
+        return new ExecutionSegment(process, startExecutionTime, completionTime);
     }
 
-    static boolean isCpuInIdleState(List<Process> sortedProcesses, Queue<Process> readyQueue,
-            Process runningProcess) {
+    public static boolean isCpuInIdleState(List<Process> sortedProcesses, Queue<Process> readyQueue,
+                                    Process runningProcess) {
         return !sortedProcesses.isEmpty() && readyQueue.isEmpty() && runningProcess == null;
     }
 
-    static void handleIdleState(SchedulingClock clock, Process firstArrivedProcess) {
+    public static void handleIdleState(SchedulingClock clock, Process firstArrivedProcess) {
         clock.setTime(firstArrivedProcess.getArrivalTime());
     }
 
-    static void handleTimeQuantumExpiration(Process process, Queue<Process> readyQueue, List<Process> processes,
-            SchedulingClock clock, int timeQuantum) {
+    public static void handleTimeQuantumExpiration(Process process, Queue<Process> readyQueue, List<Process> processes,
+                                            SchedulingClock clock, int timeQuantum) {
         clock.advance(timeQuantum);
         process.setRemainingTime(process.getRemainingTime() - timeQuantum);
         addArrivedProcessesToReadyQueue(processes, readyQueue, clock);
         readyQueue.add(process);
     }
 
-    static boolean shouldPreempt(Process activeProcess, Process arrivedProcess) {
+    public static boolean shouldPreempt(Process activeProcess, Process arrivedProcess) {
         return activeProcess != null && activeProcess.getPriority() > arrivedProcess.getPriority();
     }
 
-    static void addArrivedProcessesToReadyQueue(List<Process> sortedProcesses, Queue<Process> readyQueue,
-            SchedulingClock clock) {
+    public static void addArrivedProcessesToReadyQueue(List<Process> sortedProcesses, Queue<Process> readyQueue,
+                                                SchedulingClock clock) {
         while (!sortedProcesses.isEmpty() && clock.isBeforeOrAt(sortedProcesses.getFirst().getArrivalTime())) {
             readyQueue.add(sortedProcesses.removeFirst());
         }
