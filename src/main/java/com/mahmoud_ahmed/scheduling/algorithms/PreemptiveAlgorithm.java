fc98a1e03d6ed0d1354217be66b757dcd7e97c1f
Mahmoud Ahmed <mahmoudxbahnasawy@outlook.com>
2024-12-23 17:36:35 +0200

    feat: Introduce `PreemptiveAlgorithm` abstract class for shared preemptive scheduling logic
    - Added `PreemptiveAlgorithm` as a base class for preemptive scheduling algorithms.
    - Moved common scheduling logic (e.g., ready queue management, idle state handling) into the abstract class for reuse.
    - Defined the `shouldPreempt` abstract method to be implemented by subclasses, enabling custom preemption behavior.

---
+++ src/main/java/com/mahmoud_ahmed/scheduling/algorithms/PreemptiveAlgorithm.java
@@ -0,0 +1,56 @@
+package com.mahmoud_ahmed.scheduling.algorithms;
+
+import com.mahmoud_ahmed.model.ExecutionSegment;
+import com.mahmoud_ahmed.model.Process;
+import com.mahmoud_ahmed.model.SchedulingClock;
+import com.mahmoud_ahmed.utils.SchedulingUtil;
+
+import java.util.*;
+
+public abstract class PreemptiveAlgorithm implements SchedulingAlgorithm {
+    private final Queue<Process> readyQueue;
+
+    public PreemptiveAlgorithm(Queue<Process> readyQueue) {
+        this.readyQueue = readyQueue;
+    }
+
+    @Override
+    public List<ExecutionSegment> schedule(List<Process> processes) {
+        List<Process> sortedProcesses = SchedulingUtil.sortedProcessesByArrivalTime(processes);
+        List<ExecutionSegment> executionSegments = new LinkedList<>();
+
+        Process runningProcess = null;
+        SchedulingClock clock = new SchedulingClock();
+        int startExecutionTime = 0;
+
+        while (!sortedProcesses.isEmpty() || !readyQueue.isEmpty() || runningProcess != null) {
+            SchedulingUtil.addArrivedProcessesToReadyQueue(sortedProcesses, readyQueue, clock);
+
+            if (SchedulingUtil.isCpuInIdleState(sortedProcesses, readyQueue, runningProcess)) {
+                SchedulingUtil.handleIdleState(clock, sortedProcesses.getFirst());
+                continue;
+            }
+
+            if (!readyQueue.isEmpty() && shouldPreempt(runningProcess, readyQueue.peek())) {
+                executionSegments.add(new ExecutionSegment(runningProcess, startExecutionTime, clock.getCurrentTime()));
+                readyQueue.add(runningProcess);
+                runningProcess = null;
+            }
+
+            if (runningProcess == null && !readyQueue.isEmpty()) {
+                runningProcess = readyQueue.poll();
+                startExecutionTime = clock.getCurrentTime();
+            }
+            clock.increment();
+            runningProcess.setRemainingTime(runningProcess.getRemainingTime() - 1);
+            if (runningProcess.getRemainingTime() == 0) {
+                executionSegments.add(new ExecutionSegment(runningProcess, startExecutionTime, clock.getCurrentTime()));
+                runningProcess.resetRemainingTime();
+                runningProcess = null;
+            }
+        }
+        return executionSegments;
+    }
+
+    abstract boolean shouldPreempt(Process activeProcess, Process arrivedProcess);
+}
