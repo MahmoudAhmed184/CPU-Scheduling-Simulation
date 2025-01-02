ead6d10e1b4902dd4b75515834f443649f10d756
Mahmoud Ahmed <mahmoudxbahnasawy@outlook.com>
2025-01-02 06:28:56 +0200

    refactor: Streamline process sorting with streams, and Preserve object immutability
    - Switched from `Collections.sort` to a stream-based approach for sorting processes.
    - Ensured immutability by utilizing the `Process` copy constructor to create new instances.

--- src/main/java/com/mahmoud_ahmed/utils/SchedulingUtil.java
+++ src/main/java/com/mahmoud_ahmed/utils/SchedulingUtil.java
@@ -1,6 +1,8 @@
 package com.mahmoud_ahmed.utils;
 
 import java.util.*;
+import java.util.stream.Collectors;
+
 import com.mahmoud_ahmed.model.Process;
 
 public class SchedulingUtil {
@@ -10,8 +12,9 @@ public class SchedulingUtil {
     }
 
     public static List<Process> sortedProcessesByArrivalTime(Collection<Process> processes) {
-        List<Process> sortedProcesses = new LinkedList<>(processes);
-        Collections.sort(sortedProcesses);
-        return sortedProcesses;
+        return processes.stream()
+            .map(Process::new)
+            .sorted()
+            .collect(Collectors.toCollection(LinkedList::new));
     }
 }
